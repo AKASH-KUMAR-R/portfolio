05b60ac3d081a42d90c847118cdf3d5c3df5ac06
AKASH-KUMAR-R <akashkumar77464@gmail.com>
2025-07-11 02:20:53 +0530

    feat: project and about partially done

--- client/src/components/AboutSection.tsx
+++ client/src/components/AboutSection.tsx
@@ -1,22 +1,27 @@
-const ParagraphContent = [
-	{
-		description:
-			"I'm a creative technologist with a love for elegant problem solving. With roots in both software engineering and visual design, I aim to bring thoughtful, enduring solutions to every project.",
-	},
-	{
-		description:
-			"My portfolio reflects a commitment to clarity, function, and aesthetics—combining modern minimalism with classic sensibilities for results that last.",
-	},
-	{
-		description:
-			"Away from the screen, you'll likely find me exploring type, music, or good coffee. Download my resume or reach out to connect.",
-	},
-]
+import profileImg from "/profile.jpg";
+
+const ParagraphContent: Array<Record<"description", string>> = [];
 
 const AboutSection = () => {
 	return (
 		<div className=" w-full flex flex-col gap-6 text-primary-light">
-			<h2 className=" font-bold text-2xl ">About Me</h2>
+			<h2 className=" font-bold text-5xl ">About Me</h2>
+
+			<div className=" flex flex-col lg:flex-row lg:items-center gap-8">
+				<div className=" w-52 h-52 rounded-full overflow-hidden">
+					<img src={profileImg} className=" w-full h-full object-cover" />
+				</div>
+
+				<div className=" font-bold space-y-4 ">
+					<h2 className=" text-4xl">Full Stack Developer</h2>
+					<p className="opacity-60 max-w-lg">
+						Currently pursuing a B.Tech in Computer Science and Engineering.
+					</p>
+					<p className=" opacity-60 max-w-lg">
+						I spend my spare time turning ideas into interactive web projects.
+					</p>
+				</div>
+			</div>
 			{ParagraphContent.map((eachItem, index) => (
 				<p
 					className=" max-w-3xl text-primary-light opacity-60 font-semibold"
@@ -26,7 +31,7 @@ const AboutSection = () => {
 				</p>
 			))}
 		</div>
-	)
-}
+	);
+};
 
-export default AboutSection
+export default AboutSection;
--- client/src/components/ProjectSection.tsx
+++ client/src/components/ProjectSection.tsx
@@ -1,69 +1,96 @@
-import sparshamImage from "/sparsham.png"
-import deviMessImage from "/devi-mess.png"
-import udhymaImage from "/udhyma-project.png"
-import sapienceImage from "/sapience.png"
+import sparshamImage from "/sparsham.png";
+import deviMessImage from "/devi-mess.png";
+import udhymaImage from "/udhyma-project.png";
+import sapienceImage from "/sapience.png";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 const ProjectList = [
 	{
 		name: "Sparsham Arimpur Elderly Care Management System",
 		description: "",
 		imageUrl: sparshamImage,
 		startDate: "",
+		stack: "React, Node, Express, MongoDB",
+		role: "Full Stack",
 	},
 	{
 		name: "Mess Management System",
 		description: "",
 		imageUrl: deviMessImage,
-		startDate: "",
+		startDate: "React, Node, Express, Postgre",
+		stack: "React, Node, Express, Postgre",
+		role: "Full Stack",
 	},
 	{
 		name: "Govt Conclave Event Management System",
 		description: "",
 		imageUrl: udhymaImage,
 		startDate: "",
+		stack: "React, Node, Express, MongoDB",
+		role: "Full Stack",
 	},
 	{
 		name: "Student Tutor Management System",
 		description: "",
 		imageUrl: sapienceImage,
-		startDate: "",
+		startDate: "React, Django, Postgre",
+		role: "Frontend",
 	},
-]
+];
 
 type ProjectCardProps = {
-	name: string
-	imageUrl: string
-	projectDetails: Object
-}
+	name: string;
+	imageUrl: string;
+	projectDetails: (typeof ProjectList)[0];
+	divRef: (el: HTMLDivElement | null) => void;
+};
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
 	name,
 	imageUrl,
 	projectDetails,
+	divRef,
 }) => {
 	return (
-		<div className=" h-96 flex flex-col gap-3 rounded-md scale-95 hover:scale-100 transition-transform overflow-hidden">
+		<div
+			ref={divRef}
+			className=" h-96 flex flex-col gap-3 rounded-md scale-95 hover:scale-100 transition-transform overflow-hidden"
+		>
 			<div
-				className=" w-full h-full bg-primary-500 bg-cover bg-center "
+				className=" w-full h-full bg-primary-500 bg-cover object-center"
 				style={{
 					backgroundImage: `url(${imageUrl})`,
 				}}
 			></div>
-			<div className=" flex flex-col text-primary-light font-bold text-sm ">
+			<div className=" flex flex-col gap-1 text-primary-light font-bold text-sm ">
 				<span>{name}</span>
-				<span className=" opacity-60">May 2024</span>
+				{/* <span className=" opacity-60">May 2024</span> */}
+				<div className=" flex justify-between opacity-60">
+					<span>{projectDetails.stack}</span>
+					<span>{projectDetails.role}</span>
+				</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
 const ProjectSection = () => {
+	const containerRef = useRef<HTMLDivElement | null>(null);
+	const targetRefs = useRef<Array<HTMLDivElement | null>>([]);
+
+	useEffect(() => {}, []);
 	return (
-		<div className=" w-full ">
-			<div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+		<div ref={containerRef} className=" w-full">
+			<div className=" grid sm:grid-cols-1 lg:grid-cols-3">
 				{ProjectList.map((eachProject, index) => (
 					<ProjectCard
-						key={index}
+						key={index + eachProject.name}
+						divRef={(el) => {
+							targetRefs.current[index] = el;
+						}}
 						name={eachProject.name}
 						imageUrl={eachProject.imageUrl}
 						projectDetails={eachProject}
@@ -71,7 +98,7 @@ const ProjectSection = () => {
 				))}
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default ProjectSection
+export default ProjectSection;
